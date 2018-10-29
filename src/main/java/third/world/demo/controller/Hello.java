e6877046664674f7d7ff9a110213102adab01ea7
wx <594180536@qq.com>
2018-10-29 20:11:12 +0800

    Initial commit

---
+++ src/main/java/third/world/demo/controller/Hello.java
@@ -0,0 +1,13 @@
+package third.world.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
