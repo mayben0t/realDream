bf814a908fbaf3426ea53c01367971ed1a3e03ec
wangxiao <2355925606@qq.com>
2018-12-12 20:29:50 +0800

    通过方法反射取值 2018 12 12

---
+++ src/main/java/third/world/demo/Invoke/Run.java
@@ -0,0 +1,19 @@
+package third.world.demo.Invoke;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Author WangXiao
+ * @Description TODO
+ * @Date 2018/12/12
+ **/
+public class Run {
+
+    public static void main(String[] args) throws Exception{
+        Class c = Person.class;
+        Object o = c.newInstance();
+        Method m1 = c.getMethod("getName");
+
+        System.out.print((String)m1.invoke(o));
+    }
+}
