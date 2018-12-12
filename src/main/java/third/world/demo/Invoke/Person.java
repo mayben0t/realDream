c8b0d528fc89cc62894008e099fe994562ddda38
wangxiao <2355925606@qq.com>
2018-12-12 14:43:08 +0800

    方法反射拿到对象内部值 2018.12.12

---
+++ src/main/java/third/world/demo/Invoke/Person.java
@@ -0,0 +1,19 @@
+package third.world.demo.Invoke;
+
+/**
+ * @Author WangXiao
+ * @Description TODO
+ * @Date 2018/12/12
+ **/
+public class Person {
+
+    private String name = "xiao";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
