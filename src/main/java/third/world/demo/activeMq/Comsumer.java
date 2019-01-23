369435afa1fdce9fb4ac44cf4f42459a09f0b51c
wx <594180536@qq.com>
2019-01-23 22:49:57 +0800

    activemq学习 20190123

---
+++ src/main/java/third/world/demo/activeMq/Comsumer.java
@@ -0,0 +1,71 @@
+package third.world.demo.activeMq;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: WangX
+ * @create: 2019-01-23 22:45
+ **/
+public class Comsumer {
+
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+
+    private static final String BROKEN_URL = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+    ConnectionFactory connectionFactory;
+
+    Connection connection;
+
+    Session session;
+
+    ThreadLocal<MessageConsumer> threadLocal = new ThreadLocal<>();
+    AtomicInteger count = new AtomicInteger();
+
+    public void init(){
+        try {
+            connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_URL);
+            connection  = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void getMessage(String disname){
+        try {
+            Queue queue = session.createQueue(disname);
+            MessageConsumer consumer = null;
+
+            if(threadLocal.get()!=null){
+                consumer = threadLocal.get();
+            }else{
+                consumer = session.createConsumer(queue);
+                threadLocal.set(consumer);
+            }
+            while(true){
+                Thread.sleep(1000);
+                TextMessage msg = (TextMessage) consumer.receive();
+                if(msg!=null) {
+                    msg.acknowledge();
+                    System.out.println(Thread.currentThread().getName()+": Consumer:我是消费者，我正在消费Msg"+msg.getText()+"--->"+count.getAndIncrement());
+                }else {
+                    break;
+                }
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/third/world/demo/activeMq/Producter.java
@@ -0,0 +1,80 @@
+package third.world.demo.activeMq;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+//import javax.jms.ConnectionFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @program: demo
+ * @description: 生产者
+ * @author: WangX
+ * @create: 2019-01-23 21:51
+ **/
+public class Producter {
+
+    //ActiveMq 的默认用户名
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+    //ActiveMq 的默认登录密码
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+    //ActiveMQ 的链接地址
+    private static final String BROKEN_URL = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+    AtomicInteger count = new AtomicInteger(0);
+    //链接工厂
+    ConnectionFactory connectionFactory;
+    //链接对象
+    Connection connection;
+    //事务管理
+    Session session;
+    ThreadLocal<MessageProducer> threadLocal = new ThreadLocal<>();
+
+    public void init(){
+        try {
+            //创建一个链接工厂
+            connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_URL);
+            //从工厂中创建一个链接
+            connection  = connectionFactory.createConnection();
+            //开启链接
+            connection.start();
+            //创建一个事务（这里通过参数可以设置事务的级别）
+            session = connection.createSession(true,Session.SESSION_TRANSACTED);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String disname){
+        try {
+            //创建一个消息队列
+            Queue queue = session.createQueue(disname);
+            //消息生产者
+            MessageProducer messageProducer = null;
+            if(threadLocal.get()!=null){
+                messageProducer = threadLocal.get();
+            }else{
+                messageProducer = session.createProducer(queue);
+                threadLocal.set(messageProducer);
+            }
+            while(true){
+                Thread.sleep(1000);
+                int num = count.getAndIncrement();
+                //创建一条消息
+                TextMessage msg = session.createTextMessage(Thread.currentThread().getName()+
+                        "productor:我是生产者，我现在正在生产东西！,count:"+num);
+                System.out.println(Thread.currentThread().getName()+
+                        "productor:我是生产者，我现在正在生产东西！,count:"+num);
+                //发送消息
+                messageProducer.send(msg);
+                //提交事务
+                session.commit();
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
