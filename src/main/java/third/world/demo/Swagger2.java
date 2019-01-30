af69ce065afa83fcdb3e0d645b01a75bb18e8d28
wangxiao <2355925606@qq.com>
2019-01-30 10:35:23 +0800

    swagger测试带header 20190130

--- src/main/java/third/world/demo/Swagger2.java
+++ src/main/java/third/world/demo/Swagger2.java
@@ -11,12 +11,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger2配置类
@@ -38,12 +44,23 @@ public class Swagger2 {
      */
     @Bean
     public Docket createRestApi() {
+
+        //在配置好的配置类中增加此段代码即可
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        ticketPar.name("Authorization").description("登录校验")//name表示名称，description表示描述
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).defaultValue("Bearer ").build();//required表示是否必填，defaultvalue表示默认值
+        pars.add(ticketPar.build());//添加完此处一定要把下边的带***的也加上否则不生效
+
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("third.world.demo.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(pars);//************把消息头添加
     }
 
     /**
