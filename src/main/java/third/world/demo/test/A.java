fac9d2e15db27b0df639be4c1f71d7be4b70a2e1
wangxiao <2355925606@qq.com>
2019-02-21 09:39:41 +0800

    遍历文件夹文件数量 20190221

--- src/main/java/third/world/demo/test/A.java
+++ src/main/java/third/world/demo/test/A.java
@@ -1,9 +1,38 @@
 package third.world.demo.test;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @Author WangXiao
  * @Description TODO
  * @Date 2019/1/30
  **/
 public class A {
+
+    public static void main(String[] args) throws Exception{
+
+        File f = new File("D:\\tools\\xmind");
+        System.out.println(get(f));
+    }
+
+
+    public static int get(File f){
+        int i=0;
+        File[] files = f.listFiles();
+        if (files==null){
+            return 0;
+        }
+        for(File e:files){
+            if(e.isDirectory()){
+                i+=get(e);
+            }else {
+              i++;
+            }
+        }
+        return i;
+    }
+
+
 }
