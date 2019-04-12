b7e7e2f60d946cac972f20b14003085b55841fcf
王骁 <wangxiao@koolearn.com>
2019-04-12 18:14:28 +0800

    google guava初体验(很强大的工具包  有ratelimiter限流等操作)

---
+++ src/main/java/third/world/demo/guava/Demo.java
@@ -0,0 +1,9 @@
+package third.world.demo.guava;
+
+import com.google.common.collect.Collections2;
+
+public class Demo {
+    public static void main(String[] args) {
+        Collections2.filter(null,null);
+    }
+}
